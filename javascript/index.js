5e6a40dacf348057df187f91c795f75a271a22e8
abhishek.98 <abhishek.verma@planetspark.in>
2022-07-17 18:14:51 +0530

    Added education section

--- javascript/index.js
+++ javascript/index.js
@@ -1,11 +1,13 @@
 $(window).scroll(function () {
   if ($(window).scrollTop() > 771 && $(window).scrollTop() < 1137) {
-    console.log("testing inside top", $(window).scrollTop());
     $(".airplain_icon").css({
       top: $(window).scrollTop() - 781 + "px",
-      // "margin-top": $(window).scrollTop() + "px",
-      // "margin-left": $(window).scrollLeft() + "px",
     });
   }
-  console.log("testing height", $(window).height());
+
+  if ($(window).scrollTop() > 1429 && $(window).scrollTop() < 1780) {
+    $(".car_icon").css({
+      top: $(window).scrollTop() - 1439 + "px",
+    });
+  }
 });
